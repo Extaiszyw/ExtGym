c314bc7fbc76badfb40937d3ef93bd040da25f2a
Extaiszyw <grigoresercel0@gmail.com>
2026-04-20 18:17:39 +0300

    Delete GYM directory

--- GYM/js/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const SUPABASE_URL = 'https://gzgvyiejjlcciijovcok.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6Z3Z5aWVqamxjY2lpam92Y29rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2Nzc5MzgsImV4cCI6MjA5MjI1MzkzOH0.unzsXhOhYig_PuGjH0ZmxsIcaRuu8Z6wk4m7TztJRYM'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
